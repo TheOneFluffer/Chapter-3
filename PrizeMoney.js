50140079a7b4d46289fb7841fe664ba16388f31d
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2022-05-09 17:26:28 +0800

    Update PrizeMoney.js

--- PrizeMoney.js
+++ PrizeMoney.js
@@ -1,33 +1,37 @@
 //Allow user input
 var input = require('readline-sync');
 //creating variable
-var Rank, Other, Monies;
-Other = 6;
+var Rank, Monies;
 //Asking user to type in rank:
-Rank = input.questionInt("Please enter your rank: ");
+Rank = input.question("Please enter your rank: ");
+Rank = parseInt(Rank);
 //Switch case logic for the program
+if (isNaN(Rank))
+{
+    console.log("Please key in a number");
+}
+else
+{
 switch (Rank)
 {
-    case 0:
-        console.log("Please key in a number");
-        break;
     case 1:
-        Monies 1000;
+        Monies = 1000;
         break;
     case 2:
-        Monies 800;
+        Monies  = 800;
         break;
     case 3:
-        Monies 700;
+        Monies = 700;
         break;
     case 4:
-        Monies 300;
+        Monies = 300;
         break;
     case 5:
-        Monies 300;
+        Monies = 300;
         break;
     default:
-        Monies 20;
+        Monies = 20;
         break;
 }
 console.log("Your prize money is $" + Monies);
+}
